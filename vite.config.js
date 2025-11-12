7f496d32e6de906b439cf8ed906a47c761310f54
Maygun15 <maygun1986@gmail.com>
2025-11-12 20:58:07 +0300

    Set Vite base and gh-pages deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/hastane15/",
-  plugins: [
-    react({
-      // React 17+ için yeni JSX dönüşümünü otomatik algılar
-      jsxRuntime: "automatic",
-    }),
-  ],
-  server: {
-    port: 5174,   // istersen değiştir
-    open: true,   // dev sunucu açıldığında otomatik tarayıcı aç
-  },
+  plugins: [react()],
 });
